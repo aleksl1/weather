5b6c16374475cfbb8c0f4d7a96b1246bb86368df
aleksslawek <aleksander.slawek@mindconsulting.pl>
2024-02-07 22:34:45 +0100

    Add empty screens

---
+++ src/screens/WeatherScreen.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { Text, View } from "react-native";
+
+interface WeatherScreenProps {}
+
+const WeatherScreen: FC<WeatherScreenProps> = () => {
+  return (
+    <View>
+      <Text>Weather Screen</Text>
+    </View>
+  );
+};
+
+export default WeatherScreen;
