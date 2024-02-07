9aee3d4cd63bd709f228bd0b9aef1c92a707cba8
aleksslawek <aleksander.slawek@mindconsulting.pl>
2024-02-07 22:35:01 +0100

    Add bottom navigator

--- App.tsx
+++ App.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./src/screens/HomeScreen";
+import WeatherScreen from "./src/screens/WeatherScreen";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Text>app</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Weather"
+          component={WeatherScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="cloud" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
