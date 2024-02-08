8723818d95898d634e573a5e526de21f022d3ab7
aleksslawek <aleksander.slawek@mindconsulting.pl>
2024-02-08 22:22:53 +0100

    Navigate to weather screen

--- src/screens/WeatherScreen.tsx
+++ src/screens/WeatherScreen.tsx
@@ -1,7 +1,8 @@
-import { FC } from "react";
 import { Text, View } from "react-native";
+import { WeatherScreenType } from "../types/navigation.types";
 
-const WeatherScreen: FC = () => {
+const WeatherScreen: WeatherScreenType = ({ route: { params } }) => {
+  console.log("params", params);
   return (
     <View>
       <Text>Weather Screen</Text>
