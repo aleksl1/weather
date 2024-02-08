69c7b32730938e5aa590b962a1f7ac4029c4e9f9
aleksslawek <aleksander.slawek@mindconsulting.pl>
2024-02-08 22:08:49 +0100

    Fetch weather on home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,80 @@
-import { FC, FunctionComponent } from "react";
-import { Text, View } from "react-native";
+import { FC, useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { FetchCurrentWeatherResponse } from "../types/api.types";
+import { checkWeather } from "../utils/utils";
 
-interface HomeScreenProps {}
+const HomeScreen: FC = () => {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState<FetchCurrentWeatherResponse>();
+  const [loading, setLoading] = useState(false);
 
-const HomeScreen: FC<HomeScreenProps> = () => {
+  const handleCheckWeather = async () => {
+    setLoading(true);
+    const data = await checkWeather(city);
+    if (data) setWeatherData(data);
+    return setLoading(false);
+  };
+
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={city}
+          onChangeText={(text) => setCity(text)}
+          style={[styles.input, styles.border]}
+          placeholder="Enter city name"
+        />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button
+          color="blue"
+          title="Check Weather"
+          onPress={handleCheckWeather}
+          disabled={loading || !city}
+        />
+        <Button
+          color="#120101a3"
+          title="Remember this city"
+          onPress={() => console.log("city", city)}
+          disabled={!city}
+        />
+      </View>
+      <View>
+        <Text>{weatherData?.current.temp}</Text>
+      </View>
     </View>
   );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 32,
+    gap: 16,
+  },
+  input: {
+    width: "100%",
+    padding: 8,
+    height: 50,
+    fontSize: 25,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  border: {
+    borderColor: "blue",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+  },
+});
